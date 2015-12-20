e4028eaafe9bfae3b5ab44dff09ae6af741d2bc5
Bobinar <salva2@gmail.com>
2015-12-20 22:40:33 +0100

    Fixed precision warnings

--- SDLExperiment/SphereMesh.h
+++ SDLExperiment/SphereMesh.h
@@ -19,14 +19,15 @@ inline void push_indices(std::vector<GLushort>& indices, int sectors, int r, int
 void createSphere(std::vector<glm::vec3>& vertices, std::vector<GLushort>& indices, std::vector<glm::vec3> &normals, std::vector<glm::vec2>& texcoords,
 	float radius, unsigned int rings, unsigned int sectors)
 {
-	float const R = 1. / (float)(rings - 1);
-	float const S = 1. / (float)(sectors - 1);
+	float const R = 1.f / (float)(rings - 1);
+	float const S = 1.f / (float)(sectors - 1);
 	
-	for (int r = 0; r < rings; ++r) {
-		for (int s = 0; s < sectors; ++s) {
-			float const y = sin(-glm::half_pi<float>() + M_PI * r * R);
-			float const x = cos(2 * M_PI * s * S) * sin(M_PI * r * R);
-			float const z = sin(2 * M_PI * s * S) * sin(M_PI * r * R);
+	for (unsigned int r = 0; r < rings; ++r) {
+		for (unsigned int s = 0; s < sectors; ++s) {
+			
+			float const y = sin(-glm::half_pi<float>() + glm::pi<float>() * r * R);
+			float const x = cos(2 * glm::pi<float>() * s * S) * sin(glm::pi<float>() * r * R);
+			float const z = sin(2 * glm::pi<float>() * s * S) * sin(glm::pi<float>() * r * R);
 
 			texcoords.push_back(glm::vec2(s*S, r*R));
 			vertices.push_back(glm::vec3(x, y, z) * radius);
@@ -57,13 +58,11 @@ public:
 
 		// HACK! correct out of bounds indices! 
 		// TODO: replace sphere creation code instead of this
-		for (int i = 0; i < indices.size(); i++)
+		for (unsigned int i = 0; i < indices.size(); i++)
 		{
 			if (indices[i] >= vertices.size())
 				indices[i] = indices[i] % vertices.size();
 		}
-
-		printf("vertices = %d,  normals = %d, indices = %d\n", vertices.size(), normals.size(), indices.size());
 
 		glGenBuffers(1, &m_vertexBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
