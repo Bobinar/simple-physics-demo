80505efff928c6c422cc94d4c73f4800224028f2
Bobinar <salva2@gmail.com>
2016-02-07 19:49:20 +0100

    Added sphere to sphere collision

--- SDLExperiment/SceneSimulator.h
+++ SDLExperiment/SceneSimulator.h
@@ -52,11 +52,50 @@ public:
 		//TODO: friction
 	}
 
+	void DetectAndResolveSphereSphereCollision(Sphere& sphereA, Sphere & sphereB)
+	{
+		glm::vec3 distanceVector = sphereA.Position - sphereB.Position;
+		float distance = glm::length(distanceVector);
+
+		float radiusSum = sphereA.Radius + sphereB.Radius;
+
+		if (distance > radiusSum)
+			return;
+
+		glm::vec3 normalizedDistanceVector = distanceVector / distance;
+		glm::vec3 aToBVector = -normalizedDistanceVector;
+		glm::vec3 bToAVector = normalizedDistanceVector;
+
+		float penetrationDistance = radiusSum - distance;
+
+		sphereA.Position += penetrationDistance * bToAVector * 0.5f;
+		sphereB.Position += penetrationDistance * aToBVector * 0.5f;
+
+		float impactSpeedA = glm::dot(sphereA.Speed, aToBVector);
+		float impactSpeedB = glm::dot(sphereB.Speed, bToAVector);
+
+		const float ReboundEnergyLossFactor = 0.95f;
+		float speedCorrectionPerSphere = (impactSpeedA + impactSpeedB) * 0.5f * ReboundEnergyLossFactor;
+		glm::vec3 speedCorrectionA = (speedCorrectionPerSphere + impactSpeedA) * bToAVector;
+		glm::vec3 speedCorrectionB = (speedCorrectionPerSphere + impactSpeedB) * aToBVector;
+
+		sphereA.Speed += speedCorrectionA;
+		sphereB.Speed += speedCorrectionB;
+	}
+
 	void Update(float deltaTime, std::vector<Sphere*> &spheres)
 	{
 		for (std::vector<Sphere*>::iterator it = spheres.begin(); it != spheres.end(); ++it)
 		{
 			(*it)->Update(deltaTime);
+		}
+
+		for (std::vector<Sphere*>::iterator itA = spheres.begin(); itA != spheres.end(); ++itA)
+		{
+			for (std::vector<Sphere*>::iterator itB = itA + 1; itB != spheres.end(); ++itB)
+			{
+				DetectAndResolveSphereSphereCollision(**itA, **itB);
+			}
 		}
 
 		for (std::vector<Plane*>::iterator itA = m_planes.begin(); itA != m_planes.end(); ++itA)
