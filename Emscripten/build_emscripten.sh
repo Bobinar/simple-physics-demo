8cd234d7b360686679cbbc41feb38a8f8fccce25
Bobinar <salva2@gmail.com>
2019-10-30 01:05:25 +0100

    Using C++ 14 (for make_unique)

--- Emscripten/build_emscripten.sh
+++ Emscripten/build_emscripten.sh
@@ -1,2 +1,2 @@
 mkdir Build
-emcc ../src/main.cpp ../src/QuadMesh.cpp ../src/SceneInitialization.cpp ../src/Frustum.cpp ../src/ShaderFactory.cpp ../src/SphereMesh.cpp ../src/SceneRenderer.cpp ../src/SceneSimulator.cpp ../src/SceneManager.cpp ../src/Sphere.cpp -o Build/main.html -s USE_SDL=2 -I../lib/glm -I../include
+emcc ../src/main.cpp ../src/QuadMesh.cpp ../src/SceneInitialization.cpp ../src/Frustum.cpp ../src/ShaderFactory.cpp ../src/SphereMesh.cpp ../src/SceneRenderer.cpp ../src/SceneSimulator.cpp ../src/SceneManager.cpp ../src/Sphere.cpp -o Build/main.html -s USE_SDL=2 -I../lib/glm -I../include -std=c++1y
